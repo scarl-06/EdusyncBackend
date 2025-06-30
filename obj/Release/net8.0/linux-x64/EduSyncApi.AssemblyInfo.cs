39a37e8e5ea7a4ab9a5d808289af06ee2c8d44c7
scarl-06 <yashkumawat552@gmail.com>
2025-06-30 09:25:58 +0530

    update

--- obj/Release/net8.0/linux-x64/EduSyncApi.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/EduSyncApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduSyncApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb28d1820488af3062a0666dfca0ead28bf5dfb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07155c96e0b0c22858332119772288981f0cf649")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduSyncApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduSyncApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
